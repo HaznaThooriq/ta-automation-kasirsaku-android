573134a07e8cbc44bb4deb9380d1a7abfa87c00c
haznathooriq <haznaatthooriqoh@gmail.com>
2019-06-28 10:51:26 +0700

    Transaksi pembelian

--- src/test/java/com/yakindeveloper/kasirsaku/automationandroid/DefinitionTestSuite.java
+++ src/test/java/com/yakindeveloper/kasirsaku/automationandroid/DefinitionTestSuite.java
@@ -5,7 +5,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/KasirsakuAndroid", tags = "@Login")
+@CucumberOptions(features="src/test/resources/KasirsakuAndroid", tags = "@PembelianHappyFlow")
 //@CucumberOptions(features="src/test/resources/KasirsakuAndroid/Register.feature", tags = "@RegisterHappyFlow")
 public class DefinitionTestSuite {
 }
